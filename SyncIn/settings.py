8637e922a497a520441e4008eed7b1925cce1b29
LeFamil99 <maxgaudreau9@gmail.com>
2022-05-08 21:38:01 -0400

    Vlad georges abuzuloaie

--- SyncIn/settings.py
+++ SyncIn/settings.py
@@ -80,8 +80,14 @@ WSGI_APPLICATION = 'SyncIn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'famil',
+        'PASSWORD': 'famil',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
