7eb56eb6e67a09fb6695ec12f062af5fcc4c1e85
LeFamil99 <maxgaudreau9@gmail.com>
2022-05-12 12:02:51 -0400

    Save music info in database

--- SyncIn/settings.py
+++ SyncIn/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-t75+qmd#pkwu3i9ce+*!7e#z4_=kh_f_6ale83c^n&3vcp$wvg
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DEBUG = False
+# DEBUG = False
 
 ALLOWED_HOSTS = ["https://lit-cove-27214.herokuapp.com"]
 
