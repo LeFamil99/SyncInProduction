3f2faff0d56a6f492304da8261ffb896fee97842
LeFamil99 <maxgaudreau9@gmail.com>
2022-05-13 05:25:35 -0400

    Save music info in database

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from login.models import Profile
+from login.models import Profile, Song
 
 # Create your views here.
 
 def index(response):
+    length = len(Song.objects.all())
+
+
     # Retourn la vue de la page d'accueil
-    return render(response, "main/index.html")
+    return render(response, "main/index.html", {"length": length})
